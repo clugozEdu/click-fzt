4d12cbe6320ac82be343e986e808697c8a6fc3ee
César Lugo <clugo@fztedu.org>
2024-08-15 16:35:11 -0600

    keys supabase fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,9 @@
 // src/supabaseClient
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = "https://iemhpugvjgaazoifqjpj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllbWhwdWd2amdhYXpvaWZxanBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI3NDE4ODQsImV4cCI6MjAzODMxNzg4NH0.swftVouXudAsC5_S01v8S_YBI67oaHjz7G8ao21bOLc";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
